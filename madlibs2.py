1305b2905f1cf4a3cf8c70e05bfe69a595f735b1
BenNicholls96 <bnicholls@students.mchenry.edu>
2016-08-28 19:06:14 -0500

    Add files via upload
    PRG105-003 madlib

---
+++ madlibs2.py
@@ -0,0 +1,54 @@
+#variables
+number = 10
+adjective = purple
+animal = lions
+verb = running
+number2 = 2
+bodypart = paws
+person = daddy
+person2 = nurse
+numberminusnumber2 = 8
+number3 = 4
+number4 = 3
+number5 = 1
+animalsingle = lion
+bodypartsingle = paw
+
+
+
+
+#rhyme
+print(number + adjective + animal + verb + "on the bed")
+print(number2 + "fell off and bumped their" + bodypart)
+print(person + "called the" + person2 + ", and the" + person2 + "said,")
+print("No more" + animal + verb + "on the bed")
+
+print(verb + "on the bed," + verb + "on the bed")
+print(verb + "on the bed and falling off the bed")
+print(verb + "on the bed," + verb + "on the bed")
+print(verb + "on the bed and falling off the bed")
+
+print(numberminusnumber2 + adjective + animal + verb + "on the bed")
+print(number3 + "fell off and bumped their" + bodypart)
+print(person + "called the" + person2 + ", and the" + person2 + "said,")
+print("No more" + animal + verb + "on the bed")
+
+print(verb + "on the bed," + verb + "on the bed")
+print(verb + "on the bed and falling off the bed")
+print(verb + "on the bed," + verb + "on the bed")
+print(verb + "on the bed and falling off the bed")
+
+print(number3 + adjective + animal + verb + "on the bed")
+print(number4 + "fell off and bumped their" + bodypart)
+print(person + "called the" + person2 + ", and the" + person2 + "said,")
+print("No more" + animal + verb + "on the bed")
+
+print(verb + "on the bed," + verb + "on the bed")
+print(verb + "on the bed and falling off the bed")
+print(verb + "on the bed," + verb + "on the bed")
+print(verb + "on the bed and falling off the bed")
+
+print(number5 + adjective + animalsingle + verb + "on the bed")
+print(number4 + "fell off and bumped his" + bodypartsingle)
+print(person + "called the" + person2 + ", and the" + person2 + "said,")
+print("No more" + animal + verb + "on the bed")
